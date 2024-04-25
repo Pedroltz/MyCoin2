80eb312d8e37363a9078fc6424bea488ddf4cda2
Pedroltz <pedroluiztuninzx@gmail.com>
2024-04-25 15:15:39 -0300

    Botton NavBar and Description

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -5,9 +5,8 @@ import 'package:my_coin2/pages/guidePage.dart';
 import 'package:my_coin2/pages/searchPage.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -15,46 +14,96 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  PageController pc = PageController(
-    initialPage: 0,
-  );
+  int paginaAtual = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const GNav(
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
-          ),
-          GButton(
-            icon: Icons.info,
-            text: 'informações',
-          ),
-          GButton(
-            icon: Icons.home
-          ),
-          GButton(
-            icon: Icons.home
-          ),
-        ]
+      bottomNavigationBar: SizedBox(
+        height: 73,
+        child: GNav(
+          backgroundColor: Colors.red,
+          tabBackgroundColor: Color.fromARGB(255, 248, 93, 82),
+          activeColor: Colors.white,
+          padding: EdgeInsets.all(16),
+          color: Colors.black,
+          gap: 8,
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+              onPressed: () {
+                setState(() {
+                  paginaAtual = 0;
+                  pc.animateToPage(paginaAtual, duration: const Duration(milliseconds: 200), curve: Curves.linear);
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.book,
+              text: 'Guia',
+              onPressed: () {
+                setState(() {
+                  paginaAtual = 1;
+                  pc.animateToPage(paginaAtual, duration: const Duration(milliseconds: 200), curve: Curves.linear);
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.calculate,
+              text: 'Calculadora',
+              onPressed: () {
+                setState(() {
+                  paginaAtual = 2;
+                  pc.animateToPage(paginaAtual, duration: const Duration(milliseconds: 200), curve: Curves.linear);
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.lightbulb,
+              text: 'Dicas',
+              onPressed: () {
+                setState(() {
+                  paginaAtual = 3;
+                  pc.animateToPage(paginaAtual, duration: const Duration(milliseconds: 200), curve: Curves.linear);
+                });
+              },
+            ),
+            GButton(
+              icon: Icons.info,
+              text: 'info',
+              onPressed: () {
+                setState(() {
+                  paginaAtual = 3;
+                  pc.animateToPage(paginaAtual, duration: const Duration(milliseconds: 200), curve: Curves.linear);
+                });
+              },
+            ),
+          ],
+        ),
       ),
       appBar: AppBar(
-          centerTitle: true,
-          title: Image.asset(
-            "assets/img/MyCoinPNG.png",
-            width: 120,
-          ),
-          backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Image.asset(
+          "assets/img/MyCoinPNG.png",
+          width: 120,
         ),
+        backgroundColor: Colors.red,
+      ),
       body: PageView(
         controller: pc,
         children: const [
           GuidePage(),
           CalcPage(),
           SearchPage()
-        ],      
+        ],
       ),
     );
   }
